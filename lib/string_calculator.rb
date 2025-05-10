4be06f22f83f29843be81a9026354386448b7d72
Supriyo Mahanta <mahantasupriyo@gmail.com>
2025-05-10 12:54:43 +0530

    update code to Consider , as input numbers delimiter

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -2,7 +2,7 @@ class StringCalculator
   def add(numbers)
     return 0 if numbers.empty?
 
-    num_list = numbers.split(' ')
+    num_list = numbers.split(',')
     return num_list[0].to_i if num_list.size == 1
 
     num_list.reduce(0) { |sum, num| sum + num.to_i }
