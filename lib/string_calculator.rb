6ccc67d8258bb02a061316c821fd67c3b5cc8607
Supriyo Mahanta <mahantasupriyo@gmail.com>
2025-05-10 12:47:05 +0530

    added code for failing test: returns sum if given 2 numbers

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,10 +1,10 @@
 class StringCalculator
-
   def add(numbers)
     return 0 if numbers.empty?
 
     num_list = numbers.split(' ')
     return num_list[0].to_i if num_list.size == 1
 
+    num_list.reduce(0) { |sum, num| sum + num.to_i }
   end
 end
